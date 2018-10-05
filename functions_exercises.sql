17c1723ffc372f4d51e3251b2580fc5697350f74
Hunter-Houts <hunter.alexander.houts@gmail.com>
2018-10-05 09:02:30 -0500

    Functions exercise complete

---
+++ functions_exercises.sql
@@ -0,0 +1,6 @@
+USE employees;
+SELECT * FROM employees WHERE (first_name ='Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 1 ORDER BY first_name,last_name;
+SELECT CONCAT(first_name, ' ',last_name) FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E' order by emp_no;
+SELECT datediff(curdate() , hire_date) FROM employees WHERE hire_date LIKE '%199%' AND  birth_date LIKE '1___-12-25%' order by birth_date asc,hire_date desc;
+SELECT * FROM employees WHERE birth_date LIKE '1___-12-25%';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
