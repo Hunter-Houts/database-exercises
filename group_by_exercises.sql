7ffb930b7c6dcd11c2c2dae1592011785b7e9174
Hunter-Houts <hunter.alexander.houts@gmail.com>
2018-10-05 09:35:40 -0500

    Group by exercises complete

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -3,5 +3,4 @@ SELECT DISTINCT title FROM titles;
 SELECT CONCAT(first_name, ' ',last_name) FROM employees WHERE last_name LIKE 'E%E' GROUP BY first_name,last_name;
 SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
 SELECT COUNT(last_name) FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
-SELECT COUNT(*) FROM employees WHERE (first_name ='Irena' OR first_name = 'Vidya' OR first_name = 'Maya') ORDER BY gender;
 SELECT COUNT(*),gender FROM employees WHERE (first_name ='Irena' OR first_name = 'Vidya' OR first_name = 'Maya') GROUP BY gender;
